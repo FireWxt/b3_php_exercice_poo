48b3692040def8296edd0b40c32a64500c9ba891
FireWat <semiaosimon@gmail.com>
2026-04-07 11:30:23 +0200

    Exo 1 & 2 finis

--- exercice1.php
+++ exercice1.php
@@ -5,3 +5,28 @@
  * Ajoutez une méthode displayInfo() qui affiche les informations du livre.
  * Instanciez un livre et affichez ses informations.
 */
+
+class Book {
+    protected string $title;
+    protected string $author;
+    protected float $price;
+    
+    public function __construct(string $title, string $author, float $price) {
+        $this->title = $title;
+        $this->author = $author;
+        $this->price = $price;
+    }
+
+
+    public function displayInfo(): void {
+        echo "Title: " . $this->title . "<br>";
+        echo "Author: " . $this->author . "<br>";
+        echo "Price: $" . $this->price . "<br>";
+    }
+
+
+}
+
+
+$book = new Book("The Great Gatsby", "F. Scott Fitzgerald", 10.99);
+$book->displayInfo();
--- exercice2.php
+++ exercice2.php
@@ -7,3 +7,46 @@
 */
 
 
+class Contact {
+
+    protected string $name;
+    protected string $email;
+    protected string $phone;
+
+    public function __construct(string $name, string $email, string $phone) {
+        $this->name = $name;
+        $this->email = $email;
+        $this->phone = $phone;
+    }
+
+    public function getName(): string {
+        return $this->name;
+    }
+    public function setName(string $name): void {
+        $this->name = $name;
+    }
+    public function getEmail(): string {
+        return $this->email;
+    }
+    public function setEmail(string $email): void {
+        $this->email = $email;
+    }
+    public function getPhone(): string {
+        return $this->phone;
+    }
+    public function setPhone(string $phone): void {
+        $this->phone = $phone;
+    }
+
+    public function displayContact(): void {
+        echo "Name: " . $this->name . "<br>";
+        echo "Email: " . $this->email . "<br>";
+        echo "Phone: " . $this->phone . "<br>";
+    }
+
+
+}
+
+
+$contact = new Contact("John Doe", "john.doe@example.com", "0621345247");
+$contact->displayContact();
